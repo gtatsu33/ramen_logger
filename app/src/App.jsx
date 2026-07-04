28fa90cad7f7c7812a8c026617472b6b1b48c314
Tatsumi Sakaguchi <gtatsu33@gmail.com>
2026-07-04 09:26:22 +0900

    html 1st

---
+++ app/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h2>🍜 ラーメンロガー</h2>
+      <p>Step 1: デプロイ確認用プレースホルダー</p>
+    </div>
+  )
+}
+
+export default App
